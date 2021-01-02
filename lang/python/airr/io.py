5f6740e688d90e0a9dfe4b7d748c6379ff18837c
Laura Gutierrez Funderburk <lfunderburk@users.noreply.github.com>
2021-01-02 11:17:35 -0800

    comment airr-schema elements from cached schema

--- lang/python/airr/io.py
+++ lang/python/airr/io.py
@@ -4,7 +4,7 @@ Reference library for AIRR schema for Ig/TCR rearrangements
 from __future__ import print_function
 import sys
 import csv
-from airr.schema import RearrangementSchema, ValidationError
+#from airr.schema import RearrangementSchema, ValidationError
 
 
 class RearrangementReader:
